453eef425889ea1fe2f3f1a3c72a4115afbccfbc
td391 <td39@gmx.com>
2017-04-05 18:27:18 +0200

    - update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 setup(name='nlplib',
+      version='0.0.1',
+      url='https://github.com/td391/nlplib',
+      author='td391',
+      author_email='td39@gmx.com',
+      license='MIT',
+      platforms='any',
+      description='Python client library for langid',
       install_requires=['langid'],
       extras_require={
         'test': ['pytest'],
       },
       packages=['nlplib'])
-
